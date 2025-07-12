b14a14992bc90ac5eb425e6fd070729b7f4c8754
Ell1ott <85990359+Ell1ott@users.noreply.github.com>
2025-07-12 16:49:14 -0700

    Update index.js

--- index.js
+++ index.js
@@ -3,26 +3,26 @@
  * @param {import('probot').Probot} app
  */
 export default (app) => {
-  // Your code here
-  app.log.info("Yay, the app was loaded!");
+	// Your code here
+	app.log.info("Yay, the app was loaded!");
 
-  app.on("issues.opened", async (context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this issue!",
-    });
-    return context.octokit.issues.createComment(issueComment);
-  });
+	app.on("issues.opened", async (context) => {
+		const issueComment = context.issue({
+			body: "Thanks for opening this issue!",
+		});
+		return context.octokit.issues.createComment(issueComment);
+	});
 
-  app.on("pull_request.opened", async (context) => {
-    const issueComment = context.issue({
-      body: "Thanks for opening this pull request!",
-    });
-    return context.octokit.issues.createComment(issueComment);
-  });
+	app.on("pull_request.opened", async (context) => {
+		app.log.info("Pull request opened", context.payload);
+		const issueComment = context.issue({
+			body: "Thanks for opening this pull request!",
+		});
+		return context.octokit.issues.createComment(issueComment);
+	});
+	// For more information on building apps:
+	// https://probot.github.io/doc
 
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
+	// To get your app running against GitHub, see:
+	// https://probot.github.io/docs/development/
 };
